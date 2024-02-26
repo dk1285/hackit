d322f837917c5bdca82da31f0040cf06f893628b
Cyber DK <129024653+dk1285@users.noreply.github.com>
2024-02-26 08:20:25 +0530

    Update Fun.js

--- Fun.js
+++ Fun.js
@@ -1,1 +1,1 @@
-alert("<img src="https://i.ibb.co/gt1DyGq/IMG-20240226-081627-464.jpg" alt="IMG-20240226-081627-464" border="0"><br>chutiya banaya tum sabko")
+alert(<img src="https://i.ibb.co/gt1DyGq/IMG-20240226-081627-464.jpg" alt="IMG-20240226-081627-464" border="0"><br>"chutiya banaya tum sabko")
